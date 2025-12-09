31b63b5fa3109d3484d73a2f1ae69f9374112a49
Puspa79 <puspa.dwi.setyorini24003@mhs.uingusdur.ac.id>
2025-12-09 15:30:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Menyapa pengguna
+alert("Selamat datang di Kopi Nusantara!");
+
+// Deklarasi nama UMKM
+const namaUMKM = "Kopi Nusantara";
+console.log("Nama UMKM: " + namaUMKM);
+
+// Jumlah produk awal
+let produk = 3;
+console.log("Jumlah produk yang tersedia: " + produk);
+
+// Penambahan stok produk
+produk += 2;
+console.log("Setelah penambahan: " + produk);
+
+// Pengurangan stok karena penjualan
+produk -= 1;
+console.log("Setelah penjualan: " + produk);
+
+// Tambahan variabel lain: jumlah produk berdasarkan jenis
+let kopiArabika = 2;
+let kopiRobusta = 3;
+console.log("Jumlah Kopi Arabika: " + kopiArabika);
+console.log("Jumlah Kopi Robusta: " + kopiRobusta);
+
+// Hitung total produk semua jenis
+let totalProduk = kopiArabika + kopiRobusta;
+console.log("Total semua produk: " + totalProduk);
+
+// Menampilkan pesan penutup
+alert("Terima kasih telah berkunjung ke " + namaUMKM + "!");
+console.log("Sampai jumpa di " + namaUMKM + "!");
+
